d598bee661f893cbda9bcf97c2444253b5465d11
fliyyer <rahmatcube15@gmail.com>
2024-06-05 14:29:40 +0700

    fix

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const filmRoutes = require('./routes/filmRoutes');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
